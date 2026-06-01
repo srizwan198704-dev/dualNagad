fbc91277302190875e17ed038396648ba77d342d
SR Rizwan <srizwan198704@gmail.com>
2026-06-01 09:58:33 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
+
 buildscript {
     repositories {
         google()
@@ -9,6 +10,7 @@ buildscript {
         maven { url = uri("https://jitpack.io") }
     }
 }
-task<Delete>("clean") {
+
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
